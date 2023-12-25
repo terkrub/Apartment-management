288613857f9d9459d55be8e19e1256fe3612a743
terkrub <terkrub9@gmail.com>
2023-12-25 22:45:46 +1300

    create all status container

---
+++ apartment-management-fontend/src/components/home-page/OverallStatus.jsx
@@ -0,0 +1,46 @@
+import './OverallStatusStyles.css'
+import Icon from '../icon/StatIcon'
+
+const OverallStatus = ()=>{
+    return(
+        <div className="StatusContainer">
+            <div className="incomeStat">
+                <Icon.IncomeIcon/>
+                <div>
+                    <span>รายรับ</span>
+                    <a>xxx บาท</a>
+                </div>  
+            </div>
+            <div className="enpenseStat">
+                <Icon.ExpenseIcon/>
+                <div>
+                    <span>รายจ่าย</span>
+                    <a>xxx บาท</a>
+                </div>
+            </div>
+            <div className="profitStat">
+                <Icon.ProfitIcon/>
+                <div>
+                    <span>กำไร</span>
+                    <a>xxx บาท</a>
+                </div>
+            </div>
+            <div className="AvaRoomStat">
+                <Icon.DoorIcon/>
+                <div>
+                    <span>จำนวนห้องว่าง</span>
+                    <a>xxx ห้อง</a>
+                </div>
+            </div>
+            <div className="maintainStat">
+                <Icon.BuildIcon/>
+                <div>
+                    <span>จำนวนเรืองซ่อม</span>
+                    <a>xxx เรือง</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OverallStatus;
