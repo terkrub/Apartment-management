c9931e603e20ed596676cfc46b992ece88a367cf
terkrub <terkrub9@gmail.com>
2023-12-25 22:46:13 +1300

    create the finance chart

--- apartment-management-fontend/src/Homepage.jsx
+++ apartment-management-fontend/src/Homepage.jsx
@@ -1,9 +1,32 @@
 import Sidebar from "./components/Sidebar"
+import FinancialGraph from "./components/home-page/FinancialGraph";
+import OverallStatus from "./components/home-page/OverallStatus"
 
 const Homepage=()=>{
+    const financialData = [
+        { month: 'January', income: 1000, expense: 700, profit: 300 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'January', income: 1000, expense: 700, profit: 300 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        { month: 'February', income: 1500, expense: 800, profit: 700 },
+        // ... other months
+    ];
     return(
-        <div className="App">  
-            <Sidebar active={"home"}/>
+        <div className="App">
+            <aside>
+                <Sidebar active={"home"}/>
+            </aside>
+            <section>
+                <OverallStatus/>
+                <FinancialGraph data={financialData}/>
+            </section>
         </div>
     )
 }
---
+++ apartment-management-fontend/src/components/home-page/FinancialGraph.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { 
+    Chart as ChartJS, 
+    CategoryScale, 
+    LinearScale, 
+    BarElement, 
+    Title, 
+    Tooltip, 
+    Legend 
+} from 'chart.js';
+import './FinancialGraphStyles.css'
+
+// Register the necessary Chart.js components
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const FinancialGraph = ({ data }) => {
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                type: 'category',
+                labels: data.map(d => d.month),
+                barPercentage: 0.5,
+            },
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'ภาพรวมการเงิน ปี2024',
+                font: {
+                    size: 24
+                }
+            },
+        },
+    };
+
+    const chartData = {
+        labels: data.map(d => d.month),
+        datasets: [
+            {
+                label: 'รายรับ',
+                data: data.map(d => d.income),
+                backgroundColor: '#2AEA82',
+                borderColor: '#2AEA82',
+                borderWidth: 1,
+
+            },
+            {
+                label: 'รายจ่าย',
+                data: data.map(d => d.expense),
+                backgroundColor: '#FF5252',
+                borderColor: '#FF5252',
+                borderWidth: 1
+            },
+            {
+                label: 'กำไร',
+                data: data.map(d => d.profit),
+                backgroundColor: '#01BFFB',
+                borderColor: '#01BFFB',
+                borderWidth: 1
+            }
+        ]
+    };
+
+    return (
+        <div className='chartContainer'>
+            <Bar data={chartData} options={options} />
+        </div>
+    );
+};
+
+export default FinancialGraph;
