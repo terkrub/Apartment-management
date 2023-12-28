2b1f2b89af9156cd9d7b2ecd240d3c3fd683c022
terkrub <terkrub9@gmail.com>
2023-12-28 17:09:12 +1300

    room info status

---
+++ apartment-management-fontend/src/components/roomInfo-page/RoomStatus.jsx
@@ -0,0 +1,16 @@
+import './RoomStatusStyles.css'
+
+const RoomStatus=()=>{
+    return(
+        <div className="roomStatusContainer">
+            <div className='roomStatus'>
+                <h3>ห้องทั้งหมด:</h3>
+                <a> XXX</a>
+                <h3>จำนวนห้องว่าง:</h3>
+                <a> XXX</a>
+            </div>
+        </div>
+    )
+}
+
+export default RoomStatus
