26881997180bacb40c1ed05e5b599302c7e2fbb8
terkrub <terkrub9@gmail.com>
2023-12-28 17:10:24 +1300

    maintainance status

---
+++ apartment-management-fontend/src/components/maintain-page/MantainanceStatus.jsx
@@ -0,0 +1,14 @@
+import './MantainanceStatusStyles.css'
+
+const MantainanceStatus=()=>{
+    return(
+        <div className="maintainanceStatusContainer">
+            <div className='maintainanceStatus'>
+                <h3>จำนวนเรื่องที่ซ่อม:</h3>
+                <a> XXX</a>
+            </div>
+        </div>
+    )
+}
+
+export default MantainanceStatus
