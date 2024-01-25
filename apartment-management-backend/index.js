7afa1264332be45b0a06d52be66a250b9d7fa049
terkrub <terkrub9@gmail.com>
2024-01-25 16:12:59 +1300

    Update index.js

--- apartment-management-backend/index.js
+++ apartment-management-backend/index.js
@@ -9,12 +9,15 @@ const app = express();
 
 app.use(cors({credentials: true,
   origin: true,}));
-
+app.use(express.static(path.join(__dirname, '..', 'apartment-management-fontend', 'build')));
 
 app.use(bodyParser.json());
 
 app.use(router);
 
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '..','apartment-management-fontend', 'build', 'index.html'));
+});
 
 app.listen(getconfig.PORT,function (){
   console.log(`CORS-enabled web server listening on port ${getconfig.PORT}`);
