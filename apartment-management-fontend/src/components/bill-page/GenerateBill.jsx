05ae0ba1cf3b009e0c8dc0c047024ae1423f934e
terkrub <terkrub9@gmail.com>
2024-02-23 17:50:54 +1300

    updated address

--- apartment-management-fontend/src/components/bill-page/GenerateBill.jsx
+++ apartment-management-fontend/src/components/bill-page/GenerateBill.jsx
@@ -36,7 +36,7 @@ const GenerateBill = ({ billOption ,roomInfo, pdfContentRef, billItems}) => {
 
         <h2 style={GenerateBillStyles['billcontainer h2']}>{billOption}</h2>
         <h3 style={GenerateBillStyles['billcontainer h3']}>{branch?"บ้านลายทองรีสอร์ท":"ลายทองเฮ้าส์"}</h3>
-        <p style={GenerateBillStyles['billcontainer p']}>{branch?"888 หมู่ 15 ซอย ราชพิพัฒน์ 1 ถนนบ้านสามพร้าว อ.เมือง จ.อุดรธานี 41000":"358 หมู่ 15 ตำบลสามพร้าว อำเภอเมืองอุดรธานี จังหวัดอุดรธานี 41000"} <br/>{branch?"โทร.084-234-2222":"โทร. 0818275520 Email tertarkitty@hotmail.com ID Line tertarkitty"}</p>
+        <p style={GenerateBillStyles['billcontainer p']}>{branch?"888 หมู่ 15 ซอย ราชพัฒน์ 1 ถนนบ้านสามพร้าว อ.เมือง จ.อุดรธานี 41000":"358 หมู่ 15 ตำบลสามพร้าว อำเภอเมืองอุดรธานี จังหวัดอุดรธานี 41000"} <br/>{branch?"โทร.084-234-2222":"โทร. 0818275520 Email tertarkitty@hotmail.com ID Line tertarkitty"}</p>
 
         <div style={GenerateBillStyles['roomInfo-container']}> 
           <h3 style={GenerateBillStyles['roomInfo-container .title']}>หมายเลขห้อง:</h3>
